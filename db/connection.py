1fa003d8dc93272f14222c1d5c3e6183068b8cef
Naseer Ahmad <naseer.ahmad@mazhr.com>
2025-05-16 06:13:56 +0300

    added port for db in connection.py

--- db/connection.py
+++ db/connection.py
@@ -2,10 +2,11 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "member_db")  # Override per service with env
 DB_USER = os.getenv("DB_USER", "member_user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "secure_pw")
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
